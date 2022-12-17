bbf17044426e17d21e7725936bda567ea2b819b8
Hazel Windle <hwindle83@googlemail.com>
2022-12-17 16:12:47 +0000

    finished 1st querySelector exercise

---
+++ 01_querySelector/02_exercise/myscript.js
@@ -0,0 +1,13 @@
+////////////////  Query Selector All ////////////////////
+
+/* 
+1. Given the boiler plate use query selector console log the following items on the page:​
+
+    - The second p tag​
+    - All of the list items​ with the class list
+    - The text content of the third instance of the class “list”​
+    - The length of the list.
+
+*/
+
+// Write your JavaScript below
